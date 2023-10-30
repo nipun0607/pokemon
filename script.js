0c88d121d52d45e92ddc4cc8ecd63fa80acba50e
nipun0607 <nipunmadan0607@gmail.com>
2023-10-30 23:56:22 +0530

    changes done

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // let Api="https://pokeapi.co/api/v2/pokemon/1"
 // console.log(Api);
 let pokemon_container=document.querySelector(".pokemon_container")
+let select=document.querySelector(".type-filter");
+let filter=document.querySelector("#filter");
+let input1=document.querySelector("#pokemon-search-input");
+let color={
+  grass:"green",
+  fire:"orange",
+  water:"blue",
+}
 async function fetch_Data(){
     for (let i=1;i<=150;i++){
         let Api=`https://pokeapi.co/api/v2/pokemon/${i}`
@@ -24,7 +32,7 @@ function new_data(pokemon,i){
 
 
     let typesof=pokemon.types[0].type.name;
-    console.log(typesof);
+    // console.log(typesof);
 
     // console.log(pokemon);
     let abl=""
@@ -36,6 +44,7 @@ function new_data(pokemon,i){
 
 
     //
+    
     container.classList.add("outer_card_container");
     container.innerHTML=
     `
@@ -60,6 +69,8 @@ function new_data(pokemon,i){
     `
     pokemon_container.appendChild(container);
 
+    allcards.push(container);
+
 }
     let arrOftype=[];
 
@@ -78,3 +89,46 @@ function new_data(pokemon,i){
        
 
     }
+    let allcards=[];
+    let filterArr=[];
+    filter.addEventListener("click",(e)=>{
+      e.preventDefault();
+      let top=select.value;
+      // console.log(top);
+      allcards.forEach(element => {
+          let finalcard=element;
+        let poketype=finalcard.children[0].children[0].children[3];
+
+        // console.log(poketype);
+          // finalcard.children
+          if (poketype.innerText==top){
+              // console.log(element);
+              filterArr.push(element);
+              // console.log(filterArr);
+          }
+      });
+      // console.log(filterArr);   
+      pokemon_container.replaceChildren(...filterArr);
+      filterArr=[];
+
+    })
+    input1.addEventListener("input",(e)=>{
+      
+      let val = e.target.value;
+      // console.log(val);
+      allcards.forEach(element => {
+        finalcard=element;
+      let poketype=finalcard.children[0].children[0].children[2];
+
+      // console.log(poketype);
+        // finalcard.children
+        if (poketype.innerText.startsWith(val)){
+            // console.log(element);
+            filterArr.push(element);
+            // console.log(filterArr);
+        }
+    });
+    // console.log(filterArr);   
+    pokemon_container.replaceChildren(...filterArr);
+    filterArr=[];
+    })
